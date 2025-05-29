eea402885f4caad402a630c04f3dba0aae42d3e4
schnedann <schnedann@users.noreply.github.com>
2025-05-29 09:02:45 +0200

    more automated build with checking repository state, selecting compiler,...

---
+++ bashfu.sh
@@ -0,0 +1,280 @@
+#!/bin/bash
+
+####################################################
+# Ansi Color Codes
+####################################################
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+grey=`tput setaf 7`
+bg_red=`tput setab 1`
+bg_green=`tput setab 2`
+bg_yellow=`tput setab 3`
+bg_blue=`tput setab 4`
+bg_magenta=`tput setab 5`
+bg_cyan=`tput setab 6`
+bg_grey=`tput setab 7`
+reset=`tput sgr0`
+
+
+blink=`tput blink` # blinking text on
+bold=`tput bold`   # Select bold mode
+dim=`tput dim`	   # Select dim (half-bright) mode
+rev=`tput rev`	   # reverse video on (switch text/background colors)
+smul=`tput smul`   # Enable underline mode
+rmul=`tput rmul`   # Disable underline mode
+#rev =`tput rev`	  # Turn on reverse video mode
+smso=`tput smso`   # Enter standout (bold) mode
+rmso=`tput rmso`   # Exit standout mode
+
+
+#sitm 	  \e[3m 	  italic text on
+#ritm 	  \e[23m 	  italic text off
+#smso 	  \e[7m 	  highlighted text on
+#rmso 	  \e[27m 	  highlighted text off
+#smul 	  \e[4m 	  underlined text on
+#rmul 	  \e[24m 	  underlined text off
+#setab #1 \e[4#1m 	set background color #1 (0-7)
+#setaf #1 \e[3#1m 	set text color #1 (0-7)
+#sgr0 	  \e(B\e[m
+
+####################################################
+# Get CMake Version
+####################################################
+
+function get_cmake_version {
+  is_cmake=$(which cmake)
+  if [ -z $is_cmake ]
+  then
+    printf "$bold$yellow--> Warning, CMake is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> CMake <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_cmake
+    cmake --version
+  fi
+}
+
+####################################################
+# Get Mercurial Version
+####################################################
+
+function get_mercurial_version {
+  is_mercurial=$(which hg)
+  if [ -z $is_mercurial ]
+  then
+    printf "$bold$yellow--> Warning, Mercurial is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> Mercurial <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_mercurial
+    hg --version
+
+    #HG_VER_TMP=$(git version)
+    #HG_VER_TMP1=$(echo $HG_VER_TMP | cut -b 13)
+    #HG_VER_TMP2=$(echo $HG_VER_TMP | cut -b 15-16)
+    #HG_VER=$(expr $HG_VER_TMP1 \* "100" + $HG_VER_TMP2)
+    #printf "%s%s %s       : %s%s\n" $yellow $HG_VER_TMP $reset
+    #printf "%sVersion as Integer: %s%s\n" $yellow $HG_VER $reset
+  fi
+}
+
+####################################################
+# Get Git Version
+####################################################
+
+function get_git_version {
+  is_git=$(which git)
+  if [ -z $is_mercurial ]
+  then
+    printf "$bold$yellow--> Warning, Git is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> G i t <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_git
+    git --version
+
+    GIT_VER_TMP=$(git version)
+    GIT_VER_TMP1=$(echo $GIT_VER_TMP | cut -b 13)
+    GIT_VER_TMP2=$(echo $GIT_VER_TMP | cut -b 15-16)
+    GIT_VER=$(expr $GIT_VER_TMP1 \* "100" + $GIT_VER_TMP2)
+    printf "%s%s %s       : %s%s\n" $yellow $GIT_VER_TMP $reset
+    printf "%sVersion as Integer: %s%s\n" $yellow $GIT_VER $reset
+  fi
+}
+
+####################################################
+# Check if Git / Mercurial repository
+####################################################
+
+function get_is_git_repository {
+  inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+                    #git rev-parse --git-dir > /dev/null 2>&1
+  toplevel_git_repo="$(git rev-parse --show-toplevel 2>/dev/null)"
+
+  if [ "$inside_git_repo" ]; then
+    if test "$(pwd)" = $toplevel_git_repo; then
+      printf "$green-- root of a git repo --$reset\n"
+    else
+      printf "$yellow-- inside a git repo --$reset\n"
+    fi
+
+    if [[ $(git diff --stat) != '' ]]; then
+      printf "$red-- Repository dirty --$reset\n"
+    else
+      printf "$green-- Repository Clean --$reset\n"
+    fi
+
+  else
+    printf "$grey-- not in a git repo --$reset\n"
+  fi
+}
+
+function get_is_hg_repository {
+  inside_hg_repo="$(hg root 2>/dev/null)"
+  toplevel_hg_repo="$(hg root 2>/dev/null)"
+
+  if [ "$inside_hg_repo" ]; then
+    if test "$(pwd)" = $toplevel_hg_repo; then
+      printf "$green-- root of a hg repo --$reset\n"
+    else
+      printf "$yellow-- inside a hg repo --$reset\n"
+    fi
+
+    if [ -n "$(hg status -mar)" ]; then
+      printf "$red-- Repository dirty --$reset\n"
+    else
+      printf "$green-- Repository Clean --$reset\n"
+    fi
+
+  else
+    printf "$grey-- not in a hg repo --$reset\n"
+  fi
+}
+
+####################################################
+# Get Gcc Version
+####################################################
+
+function get_gcc_version {
+  is_gcc=$(which gcc)
+  if [ -z $is_gcc ]
+  then
+    printf "$bold$yellow--> Warning, Gnu C Compiler is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> Gnu C Compiler <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_gcc
+    gcc --version
+  fi
+}
+
+####################################################
+# Get Clang Version
+####################################################
+
+function get_clang_version {
+  is_clang=$(which clang)
+  if [ -z $is_clang ]
+  then
+    printf "$bold$yellow--> Warning, Clang C Compiler is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> Clang C Compiler <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_clang
+    clang --version
+  fi
+}
+
+####################################################
+# Get G++ Version
+####################################################
+
+function get_gpp_version {
+  is_gpp=$(which g++)
+  if [ -z $is_gpp ]
+  then
+    printf "$bold$yellow--> Warning, Gnu C++ Compiler is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> Gnu C++ Compiler <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_gpp
+    g++ --version
+  fi
+}
+
+####################################################
+# Get Clang++ Version
+####################################################
+
+function get_clangpp_version {
+  is_clangpp=$(which clang++)
+  if [ -z $is_clangpp ]
+  then
+    printf "$bold$yellow--> Warning, Clang C++ Compiler is not installed <--$reset\n"
+  else
+    printf "$bold$grey--> Clang C++ Compiler <--$reset\n"
+    printf "$grey--> %s <--$reset\n" $is_clangpp
+    clang++ --version
+  fi
+}
+
+####################################################
+# Test/Ensure target dir exists
+####################################################
+
+function test_ensure_folder {
+printf "%sTest Ensure Folder: %s%s\n" $grey $1 $reset
+if test ! -d $1
+then
+  mkdir $1
+fi
+
+cd $1
+pwd
+}
+
+####################################################
+# Clone Fetch with git
+####################################################
+
+function clone_fetch_git {
+if [ ! -d $1 ]
+then
+  printf "%sCloning...%s\n" $green $reset
+  git clone $2 --branch $3;
+else
+  printf "%sFetching...%s\n" $green $reset
+  cd $1;
+  pwd;
+  #git fetch $2 --tags --progress $3;
+  git fetch --tags --progress --all;
+  cd ..;
+fi
+}
+
+####################################################
+# git info
+####################################################
+
+GITLOLA="log --graph --decorate --pretty=oneline --abbrev-commit --all --date=local -n 20"
+GITLOL="log --graph --decorate --pretty=oneline --abbrev-commit -n 20"
+
+function git_info_about_repository {
+cd $1
+printf "%s---> Remotes <---%s\n" $magenta $reset
+git remote -v
+printf "%s---> Branches <---%s\n" $magenta $reset
+git branch -va
+printf "%s---> Tags <---%s\n" $magenta $reset
+git tag -n5
+printf "%s--> latest Tag <--%s\n" $grey $reset
+git describe --tags
+printf "%s---> Status <---%s\n" $magenta $reset
+git status;
+#git log --stat -n 1
+git $GITLOLA
+cd ..;
+}
+
+####################################################
+# git graph
+####################################################
--- build.sh
+++ build.sh
@@ -1,5 +1,154 @@
 #!/bin/bash
 
+clear
+
+####################################################
+# Load Script-Fu
+####################################################
+
+source ./bashfu.sh
+echo "$reset"
+
+####################################################
+# Load Build Config
+####################################################
+
+source ./build_config.cfg
+
+####################################################
+
+# Set the timeout duration in seconds
+timeout_duration=3
+
+####################################################
+# ensure build directory exits
+####################################################
+
+test_ensure_folder build
+cd ..
+
+####################################################
+# Test for Executables
+####################################################
+
+printf "$bold$green--> T E S T   F O R   E X E C U T A B L E S <--$reset\n"
+
+#which hg 2>/dev/null || printf "$yellow--> Warning, Mercurial is not installed <--$reset\n"
+
+#----- CMake
+
+get_cmake_version
+
+#----- Git / Mercurial versions
+
+get_mercurial_version
+get_git_version
+
+#----- C Compiler
+
+get_gcc_version
+get_clang_version
+
+printf "$bold$yellow--> Selected C Compiler <--$reset\n"
+#echo $prefer_clang_over_gcc
+#echo $is_gcc
+#echo $is_clang
+is_c_compiler=$is_gcc
+if [ ! -z $prefer_clang_over_gcc ]
+then
+  if [ ! -z $is_clang ]
+  then
+    is_c_compiler=$is_clang
+  fi
+fi
+printf "$grey--> %s <--$reset\n" $is_c_compiler
+
+#----- C++ Compiler
+
+get_gpp_version
+get_clangpp_version
+
+printf "$bold$yellow--> Selected C++ Compiler <--$reset\n"
+is_cpp_compiler=$is_gpp
+if [ ! -z $prefer_clang_over_gcc ]
+then
+  if [ ! -z $is_clangpp ]
+  then
+    is_cpp_compiler=$is_clangpp
+  fi
+fi
+printf "$grey--> %s <--$reset\n" $is_cpp_compiler
+
+#----- Linker
+
+is_ldgnu=$(which ld)
+is_ldgold=$(which ld.gold)
+is_ldclang=$(which ld.lld)
+
+is_sel_linker=$is_ldgnu
+
+#echo $is_ldgnu
+#echo $is_ldgold
+#echo $is_ldclang
+
+if [ ! -z $is_ldclang ];then
+  printf "$bold$yellow--> Selected the Clang Linker <--$reset\n"
+  printf "$grey--> %s <--$reset\n" $is_ldclang
+  is_sel_linker=$is_ldclang
+elif [ ! -z $is_ldgold ];then
+  printf "$bold$yellow--> Selected the Gold Linker <--$reset\n"
+  printf "$grey--> %s <--$reset\n" $is_ldgold
+  is_sel_linker=$is_ldgold
+else
+  printf "$bold$yellow--> Selected the Gnu Linker <--$reset\n"
+  printf "$grey--> %s <--$reset\n" $is_ldgnu
+fi
+
+#echo $is_sel_linker
+$is_sel_linker --version
+
+#-----
+
+# Prompt the user
+echo "Press any key to continue or wait for $timeout_duration seconds..."
+# Use read with a timeout
+read -t $timeout_duration -n 1 key
+
+#-----
+
+####################################################
+# Some Repository Information
+####################################################
+
+printf "$bold$green--> R E P O S I T O R Y   S T A T U S <--$reset\n"
+
+if [ ! -z $is_git ]; then
+  get_is_git_repository
+fi
+if [ ! -z $is_mercurial ]; then
+  get_is_hg_repository
+fi
+
+if [ ! -z $inside_git_repo ]; then
+  git status
+fi
+if [ ! -z $inside_hg_repo ]; then
+  hg summary
+fi
+
+#-----
+
+# Prompt the user
+echo "Press any key to continue or wait for $timeout_duration seconds..."
+# Use read with a timeout
+read -t $timeout_duration -n 1 key
+
+#-----
+
+####################################################
+# prepare Build-Directory
+####################################################
+
 # Specify the directory you want to check
 DIR="./build"
 
@@ -14,12 +163,34 @@ fi
 
 rm -rf $DIR/*
 
+####################################################
+# Build
+####################################################
+
+printf "$bold$green--> B U I L D I N G   A L L   T A R G E T S <--$reset\n"
+
+printf "$cyan--> Generate Makefile <--$reset\n"
+#cmake -G "Your Generator"  path/to/your/source
+
+if [ -n "$is_clangpp" ]; then
 cmake -S . -B $DIR --trace-expand\
-      -DCMAKE_C_COMPILER=clang \
-      -DCMAKE_CXX_COMPILER=clang++ \
+      -G $use_build_system\
+      -DCMAKE_C_COMPILER=$is_c_compiler\
+      -DCMAKE_CXX_COMPILER=$is_cpp_compiler\
+      -DCMAKE_LINKER=$is_sel_linker\
+      -DCMAKE_COLOR_DIAGNOSTICS=ON\
+      -DCMAKE_BUILD_TYPE=Release
+else
+cmake -S . -B $DIR --trace-expand\
+      -G $use_build_system\
+      -DCMAKE_C_COMPILER=$is_c_compiler\
+      -DCMAKE_CXX_COMPILER=$is_cpp_compiler\
+      -DCMAKE_LINKER=$is_sel_linker\
       -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
       -DCMAKE_EXE_LINKER_FLAGS="-stdlib=libc++" \
+      -DCMAKE_COLOR_DIAGNOSTICS=ON\
       -DCMAKE_BUILD_TYPE=Release
+fi
 
 # Check if the command failed
 if [ $? -ne 0 ]; then
@@ -27,14 +198,26 @@ if [ $? -ne 0 ]; then
     echo "Cmake failed."
 fi
 
-cmake --build $DIR -- -j4
+printf "$cyan--> Compile N Link <--$reset\n"
+cmake --build $DIR --config Release --clean-first
+##cmake --build $DIR --config Release --clean-first -- --stop-on-first-error -j
 
 # Check if the command was successful
 if [ $? -eq 0 ]; then
     # Command succeeded, execute further commands
     echo "The build was successful."
-    ls -l $DIR/bin
+    ls -lh $DIR/bin
 else
     # Command failed, handle the error
     echo "The build failed."
 fi
+
+####################################################
+# Goodbye
+####################################################
+
+printf "$bold$green--> F I N I S H E D <--$reset\n"
+
+echo "$reset"
+
+pwd
